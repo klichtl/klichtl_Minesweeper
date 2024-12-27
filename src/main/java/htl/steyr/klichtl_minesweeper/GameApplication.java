f062e1702989079f058edf2f274830dab4165733
Kevin Lichtl <lichtl.kevin@gmail.com>
2024-12-27 23:17:34 +0100

    changed from 2 fxml files to 1

--- src/main/java/htl/steyr/klichtl_minesweeper/GameApplication.java
+++ src/main/java/htl/steyr/klichtl_minesweeper/GameApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("menu-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Minesweeper");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/Minesweeper-Icon.png")));
--- src/main/java/htl/steyr/klichtl_minesweeper/GameController.java
+++ src/main/java/htl/steyr/klichtl_minesweeper/GameController.java
@@ -1,49 +1,58 @@
 package htl.steyr.klichtl_minesweeper;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.GridPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
     @FXML
+    public GridPane Grid;
+    @FXML
     public ChoiceBox<String> chosenDifficulty;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        /* Set the values for the ChoiceBox (Difficulty) */
-        chosenDifficulty.getItems().addAll("Beginner", "Advanced", "Professional");
-
+        chosenDifficulty.setItems(FXCollections.observableArrayList("Beginner", "Advanced", "Professional"));
+        chosenDifficulty.setValue("Beginner");
     }
 
-    public void onStartGameButtonClicked(ActionEvent actionEvent) {
+    public String getDifficulty() {
+        String difficultyPath = "";
 
-        /* See what difficulty was chosen, and therefore, change what grid is displayed */
+        String difficulty = chosenDifficulty.getValue();
 
-//        switch (getChosenDifficulty()) {
-//            case "Beginner":
-//
-//                break;
-//            case "Advanced":
-//
-//                break;
-//            case "Professional":
-//
-//                break;
-//        }
+        switch (difficulty) {
+            case "Beginner":
+                difficultyPath = "/htl/steyr/klichtl_minesweeper/difficultys/beginner-Grid-view.fxml";
+                break;
+            case "Advanced":
+                difficultyPath = "/htl/steyr/klichtl_minesweeper/difficultys/advanced-Grid-view.fxml";
+                break;
+            case "Professional":
+                difficultyPath = "/htl/steyr/klichtl_minesweeper/difficultys/professional-Grid-view.fxml";
+                break;
+            default:
+                throw new IllegalStateException("Error loading Difficulty");
+        }
 
+        return difficultyPath;
     }
 
-    public ChoiceBox<String> getChosenDifficulty() {
-        return chosenDifficulty;
-    }
+    @FXML
+    public void onStartButtonClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loadGrid = new FXMLLoader(getClass().getResource(getDifficulty()));
+        GridPane selectedGrid = loadGrid.load();
 
-    public void setChoseDifficulty(ChoiceBox<String> choseDifficulty) {
-        this.chosenDifficulty = choseDifficulty;
+        Grid.getChildren().clear();
+        Grid.add(selectedGrid, 0, 0);
     }
 }
